9c0b0d7a208f1e336cda59e3a9dbe96fd3d37e55
ubertrickster <ubertrickster@gmail.com>
2020-07-19 19:24:31 +0300

    first commit

---
+++ Main.cpp
@@ -0,0 +1,24 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(500, 300), "test");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Red);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
